e738994c80a342835df14b450ad29f465e9c49cd
Felipe Pereira <felipe.dspereira@gmail.com>
2020-06-26 00:09:09 +0200

    exposing as endpoint

---
+++ src/main/java/com/felipe/rest/CalendarResource.java
@@ -0,0 +1,30 @@
+package com.felipe.rest;
+
+import static org.springframework.format.annotation.DateTimeFormat.ISO.DATE_TIME;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.felipe.domain.TimeSlot;
+import com.felipe.service.CalendarService;
+
+@RestController()
+public class CalendarResource {
+
+  @Autowired private CalendarService service;
+
+  @GetMapping("calendar/availableTime")
+  public List<TimeSlot> findAvailableTime(
+      @RequestParam("calendarIds") List<String> calendarIds,
+      @RequestParam("meetingDuration") Integer meetingDuration,
+      @RequestParam("startPeriod") @DateTimeFormat(iso = DATE_TIME)
+          LocalDateTime startPeriod,
+      @RequestParam("endPeriod") @DateTimeFormat(iso = DATE_TIME)
+          LocalDateTime endPeriod) {
+
+    return service.findAvailableTime(calendarIds, meetingDuration, startPeriod, endPeriod);
+  }
+}
